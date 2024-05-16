9acf93e45c8a937ad530a53ade75d48c90802188
Kunwar Sarthak Singh <kuwarsarthaksingh735@gmail.com>
2024-05-16 09:32:37 +0530

    Completed the add and list component

--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { toast } from 'react-toastify';
 
-const List = () => {
+const List = ({url}) => {
+
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+    const response = await axios.get(`${url}/api/food/list`);
+    if (response.data.success) {
+      setList(response.data.data);
+    }else{
+      toast.error("Error");
+    }
+  }
+
+  const removeFood = async(foodId) => {
+    const response = await axios.post(`${url}/api/food/remove`,{id:foodId});
+    await fetchList();
+    if (response.data.success) {
+      toast.success(response.data.message)
+    }else{
+      toast.error("Error")
+    }
+  }
+
+  useEffect(() => {
+    fetchList();
+  },[])
+
   return (
-    <div>
-      list
+    <div className=' py-[20px] px-[45px] '>
+      <p>All Food List</p>
+      <div>
+        <div className=' grid grid-cols-[_0.5fr_2fr_1fr_1fr_0.5fr] items-center gap-[10px] py-[12px] px-[15px] border-[1px] border-solid border-[#cacaca] text-[13px] bg-[#f9f9f9] '>
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+        {list.map((item,index) => {
+          return(
+            <div key={index} className=' grid grid-cols-[_0.5fr_2fr_1fr_1fr_0.5fr] items-center gap-[10px] py-[12px] px-[15px] border-[1px] border-solid border-[#cacaca] text-[13px] '>
+              <img src={`${url}/images/`+item.image} alt="" />
+              <p>{item.name}</p>
+              <p>{item.category}</p>
+              <p>${item.price}</p>
+              <p onClick={() => removeFood(item._id)} className=' cursor-pointer '>X</p>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
